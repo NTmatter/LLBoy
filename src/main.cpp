9fe5d9f3f384b0d1f7d290274bb6bb87576ad68c
Thomas Johnson <NTmatter@gmail.com>
2011-01-08 05:42:38 +0100

    Importing LLVM headers and linking correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+#include <llvm/Target/TargetSelect.h>
+
 using namespace std;
+using namespace llvm;
 
 int main(int argc, char** argv)
 {
     cout << "Hello, cruel world!" << endl;
+    InitializeNativeTarget();
+    cout << "Goodbye." << endl;
     return 0;
 }

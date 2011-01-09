34dae3d66069002bf44a4f61bcf374980b6a7f24
Thomas Johnson <NTmatter@gmail.com>
2011-01-09 18:31:00 +0100

    WIP: Loading LLVM modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 
-#include <llvm/Target/TargetSelect.h>
+#include <llvm/LLVMContext.h>
+#include <llvm/Module.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+
 
 using namespace std;
 using namespace llvm;
 
+Module* read_module(LLVMContext* ctx, string path, string* error)
+{
+    MemoryBuffer* buffer;
+    Module* out = NULL;
+    if((buffer = MemoryBuffer::getFile(path, error)))
+    {
+        out = ParseBitcodeFile(buffer, *ctx);
+        delete buffer;
+    } else {
+        cerr << "Could not find file " << path << endl;
+    }
+    return out;
+}
+
 int main(int argc, char** argv)
 {
-    cout << "Hello, cruel world!" << endl;
-    InitializeNativeTarget();
-    cout << "Goodbye." << endl;
+    string error;
+    MemoryBuffer* buffer;
+    LLVMContext* context = &(getGlobalContext());
+ 
+    cout << "Loading System and CPU code...";   
+    Module* module_cpu = read_module(context, "sys/cpu.o", &error);
+    Module* module_mmu = NULL;
+    Module* module_system = read_module(context, "sys/system.o", &error);
+    cout << "Complete." << endl;
+    
     return 0;
 }

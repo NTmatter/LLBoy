030baf8ae9afb35ee8ac10013dbb48fdfca1a911
Thomas Johnson <NTmatter@gmail.com>
2011-02-08 22:49:19 +0100

    Applying MIT license to sources

--- src/cmake_modules/FindLLVM.cmake
+++ src/cmake_modules/FindLLVM.cmake
@@ -1,8 +1,28 @@
+# Copyright (c) 2011 Thomas Johnson
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 # Find the full LLVM package using LLVM_ROOT as a hint
 # Reference Material:
 # http://eris.liralab.it/wiki/CMake_and_FIND_PACKAGE
 # http://www.cmake.org/cgi-bin/viewcvs.cgi/Modules/readme.txt?root=CMake&view=markup
-
 set(LLVM_ROOT "" CACHE PATH "Root of LLVM install.")
 if(NOT EXISTS ${LLVM_ROOT}/include/llvm)
     message(FATAL_ERROR "LLVM_ROOT (${LLVM_ROOT}) is not a valid LLVM install.")

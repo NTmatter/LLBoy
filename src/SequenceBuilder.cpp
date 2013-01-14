023b26ba9095f9591b6c28cb82541e8dd107d53a
Thomas Johnson <NTmatter@gmail.com>
2013-01-14 03:47:57 -0800

    WIP: Translator - Outline of instruction emitter logic

--- src/SequenceBuilder.cpp
+++ src/SequenceBuilder.cpp
@@ -19,6 +19,9 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
+#import <iostream>
+#import <iomanip>
+
 #import "SequenceBuilder.h"
 #import <llvm/Support/system_error.h>
 #import <llvm/Support/MemoryBuffer.h>
@@ -27,11 +30,16 @@
 #import <llvm/PassManager.h>
 #import <llvm/Analysis/Verifier.h>
 #import <llvm/LinkAllPasses.h>
+#import <llvm/BasicBlock.h>
 
 extern "C" {
 #import "sys/cpu.h"
 #import "sys/cpu_functions.h"
+#import "cart_metadata.h"
+#import "sys/bios.h"
 }
+
+using namespace std;
 
 Module* readModule(LLVMContext* ctx, std::string path, std::string moduleName)
 {
@@ -47,9 +55,151 @@ Module* readModule(LLVMContext* ctx, std::string path, std::string moduleName)
     return ParseBitcodeFile(mb.get(), *ctx);
 }
 
+/// Mockup of the generated execution function
+uint32_t exampleExecuteNative(system_t* sys)
+{
+entry:
+    uint16_t pc = sys->cpu.pc;
+    uint32_t execution_count = 0;
+    
+    switch (pc) {
+        case 0x0000:
+            cpu_op_LDSPnn(sys);
+            execution_count += sys->cpu.m;
+            cpu_op_XORr_a(sys);
+            execution_count += sys->cpu.m;
+        case 0x0007:
+            cpu_op_LDHLDA(sys); // landing == true
+            execution_count += sys->cpu.m;
+            cpu_op_BIT7h(sys);
+            execution_count += sys->cpu.m;
+            cpu_op_JRNZn(sys);
+            execution_count += sys->cpu.m; // terminator == true
+            break;
+        default:
+            break;
+    }
+    
+end:
+    return execution_count;
+}
+
 void SequenceBuilder::buildFromCart(Module* m, system_t* sys)
 {
+    Function* execute = m->getFunction("execute");
+    BasicBlock* entry = BasicBlock::Create(m->getContext(), "entry", execute);
+    BasicBlock* finish = BasicBlock::Create(m->getContext(), "finish", execute);
+    SwitchInst* sw;
+    
+    if(execute) execute->eraseFromParent();
+    
+    // Create Execute function from scratch
+    {
+        StructType* system_s = m->getTypeByName("struct.system_t");
+        PointerType* pSystem_s = PointerType::getUnqual(system_s);
+        FunctionType* ft = FunctionType::get(IntegerType::getInt32Ty(m->getContext()),
+                                             pSystem_s,
+                                             (Type*)0);
+        
+        execute = m->getOrInsertFunction("execute", ft);
+
+        // Populate entry block:
+        //   get PC
+        //   create execution cycle accumulator
+        //   create switch(pc) defaulting to exit block
+        // Populate finish block
+        //   return execution cycle accumulator
+        
+        // Create translated blocks, inserting before "finish":
+        BasicBlock* current = BasicBlock::Create(m->getContext(), "", execute, finish);
+        // Add "current" to the switch and begin populating the block
+        // On landings, create a "next" block, add an unconditional jump from previous to next
+        // On terminations, create an unconditional jump to finish block, then scan for new instructions
+    }
+    
+    BasicBlock* b;
     // BIOS 0x0000-0x00FF
+    for(int i = 0; i < 0x100; i++)
+    {
+        const cart_metadata_s md = bios_metadata[i];
+        
+        // Debugging info
+        if(md.isVisited && md.isInstruction)
+        {
+            cpu_op_metadata_s op_md = cpu_op_metadata_basic[bios[i]];
+            if(op_md.opcode == 0xCB) op_md = cpu_op_metadata_cb[bios[++i]];
+            
+            // Display op info
+            if(md.isLanding && op_md.terminator)
+            {
+                cout << "TL";
+            } else if(md.isLanding) {
+                cout << " L";
+            } else if(op_md.terminator) {
+                cout << "T ";
+            } else {
+                cout << "  ";
+            }
+            cout << setw(2) << hex << setfill('0') << i << ": " << op_md.name << "(";
+            if(op_md.args >= 1) cout << "0x" << setw(2) << hex << (unsigned int)bios[i + 1];
+            if(op_md.args == 2) cout << ", 0x" << setw(2) << hex << (unsigned int)bios[i + 2];
+            cout << ")";
+            
+            cout << endl;
+        }
+        
+        // Unvisited -> Unvisited: continue
+        // Unvisited -> Visited: new block
+        // Visited -> Visited: new block if landing or terminator.
+        // Visited -> Unvisited: break, jump to end. Set previous to null.
+        
+        if(md.isVisited && md.isInstruction)
+        {
+            // Chain blocks together if needed for a landing instruction.
+            // If there is no previous block, there is no need for chaining.
+            if(md.isLanding && b)
+            {
+                BasicBlock* previous = b;
+                b = BasicBlock::Create(m->getContext(), "", execute, finish);
+                BranchInst::Create(b, previous);
+                // Then add to switch for current offset (Beware with 0xCB instructions)
+                // sw->addCase(i, b);
+            }
+            
+            // Ensure that a current block exists
+            if(!b)
+            {
+                b = BasicBlock::Create(m->getContext(), "", execute, finish);
+                // Then add to switch for current offset (Beware with 0xCB instructions)
+                // sw->addCase(i, b);
+            }
+            
+            // Retrieve metadata about the currently-selected op
+            cpu_op_metadata_s op_md = cpu_op_metadata_basic[bios[i]];
+            if(op_md.opcode == 0xCB) op_md = cpu_op_metadata_cb[bios[++i]];
+            
+            // Create call instruction
+            Function* op = m->getFunction(op_md.impl_name);
+            // CallInst::Create(op, ArrayRef<PointerType>(/* Function's sys_t argument */), "", b);
+            
+            // Terminate the block if necessary
+            if(op_md.terminator)
+            {
+                BranchInst::Create(finish, b);
+                b = NULL;
+            }
+        }
+        
+        // Moving into an unvisited region of memory. Terminate the block.
+        if(!md.isVisited && b)
+        {
+            BranchInst::Create(finish, b);
+            b = NULL;
+        }
+    }
+    
+    // Finally, finish an unterminated block if at the end of the memory region:
+    if(b) BranchInst::Create(finish, b);
     
     // Reserved Memory 0x0000-0x00FF
     
@@ -58,7 +208,7 @@ void SequenceBuilder::buildFromCart(Module* m, system_t* sys)
     // Switchable ROM bank 0x4000-0x7FFF
 }
 
-void SequenceBuilder::optimize(Module* m)
+bool SequenceBuilder::optimize(Module* m)
 {
     PassManager pm;
     pm.add(new DataLayout(m));
@@ -105,9 +255,9 @@ void SequenceBuilder::optimize(Module* m)
     pm.add(createLoopIdiomPass());
     pm.add(createLoopDeletionPass());
     pm.add(createLoopUnrollPass());
-    pm.add(createMemDepPrinter()); // -memdep?
+    // pm.add(createMemDepPrinter()); // -memdep?
     pm.add(createGVNPass());
-    pm.add(createMemDepPrinter());
+    // pm.add(createMemDepPrinter());
     pm.add(createMemCpyOptPass());
     pm.add(createSCCPPass());
     pm.add(createInstructionCombiningPass());
@@ -115,7 +265,7 @@ void SequenceBuilder::optimize(Module* m)
     pm.add(createJumpThreadingPass());
     pm.add(createCorrelatedValuePropagationPass());
     // -domtree?
-    pm.add(createMemDepPrinter());
+    // pm.add(createMemDepPrinter());
     pm.add(createDeadStoreEliminationPass());
     pm.add(createAggressiveDCEPass());
     pm.add(createCFGSimplificationPass());
@@ -126,5 +276,5 @@ void SequenceBuilder::optimize(Module* m)
     // -domtree?
     pm.add(createVerifierPass());
     
-    pm.run(*m);
+    return pm.run(*m);
 }
